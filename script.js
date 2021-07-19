bb2f9b590708cfe815c34241afca13af61044d85
Tanay <tanay.joshi9@gmail.com>
2021-07-19 21:03:53 +0530

    cleaned the code

--- script.js
+++ script.js
@@ -1,152 +1,194 @@
-const playerFactory = (name,sign) => {
-    return {name,sign};
-}
+
 
 const gameboard = (() => {
-    // let playerOne = '';
-    // let playerTwo = '';
+    let playerOne = '';
+    let playerTwo = '';
     let gameboardArray = ['','','','','','','','',''];
+    let selectedPlayer = '';
     
-
-    const playBtn = document.getElementById('playBtn');
-
-    const playerNameInput = () => {
+    const playerObj = (name,sign) => {
+       return {name,sign};
+    }
+    const setPlayer = () => {
         let input1 = document.getElementById('player1');
         let input2 = document.getElementById('player2');
-        let playerOne = playerFactory(input1.value,'x');
-        let playerTwo = playerFactory(input2.value,'o');
-        return {input1,input2,playerOne,playerTwo}
+
+        playerOne = playerObj(input1.value,'x');
+        playerTwo = playerObj(input2.value,'o');
+        
+        return {playerOne,playerTwo}
     }
 
+    // alternate turns for players
     const selectPlayer = (num) => {
-       if(num % 2 == 0){
-            return playerNameInput().playerOne;
-        }else return playerNameInput().playerTwo;
+        if(num % 2 == 0){
+            selectedPlayer = playerOne;
+        }else selectedPlayer = playerTwo;
+        return selectedPlayer;
     }
 
+    //declare winner
     const result = () => {
+        let winMsg = selectedPlayer.name + ' wins!';
+        let drawMsg = "It's a draw!";
         //function to check equality of gameboardArray items
         function areEqual(){
             var len = arguments.length;
             for (var i = 1; i< len; i++){
-               if (arguments[i] === null || arguments[i] !== arguments[i-1])
+               if (arguments[i] === '' || arguments[i] !== arguments[i-1])
                   return false;
             }
             return true;
          }
+         
         if(areEqual(gameboardArray[0],gameboardArray[1],gameboardArray[2])){
-            if(gameboardArray[0] == 'x'){
-                
+            return winMsg;
+        }else if(areEqual(gameboardArray[3],gameboardArray[4],gameboardArray[5])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[6],gameboardArray[7],gameboardArray[8])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[0],gameboardArray[3],gameboardArray[6])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[1],gameboardArray[4],gameboardArray[7])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[2],gameboardArray[5],gameboardArray[8])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[0],gameboardArray[4],gameboardArray[8])){
+            return winMsg;
+        }else if(areEqual(gameboardArray[2],gameboardArray[4],gameboardArray[6])){
+            return winMsg;
+        }else{
+            for(let i = 0; i < 9; i++){
+                if(gameboardArray[i] == ''){
+                    return;
+                }
             }
-        }else if(gameboardArray[0],gameboardArray[1],gameboardArray[2]){
-
-        }else if(gameboardArray[3],gameboardArray[4],gameboardArray[5]){
-            
-        }else if(gameboardArray[6],gameboardArray[8],gameboardArray[8]){
-            
-        }else if(gameboardArray[0],gameboardArray[3],gameboardArray[6]){
-            
-        }else if(gameboardArray[1],gameboardArray[4],gameboardArray[7]){
-            
-        }else if(gameboardArray[2],gameboardArray[5],gameboardArray[8]){
-            
-        }else if(gameboardArray[0],gameboardArray[4],gameboardArray[8]){
-            
-        }else if(gameboardArray[2],gameboardArray[4],gameboardArray[6]){
-            
+            return drawMsg;
         }
     }
 
-    return {playBtn,gameboardArray,playerNameInput,selectPlayer,result}
+    return {playerOne,playerTwo,gameboardArray,setPlayer,selectPlayer,result}
 })();
   
    
 
 
-
 const displayController = () => {
-    const gameboardGrid = document.querySelector('.gameboard');
-
-
+    const gameboardGridContainer = document.querySelector('.gameboard-container');
+    const turnDisplay = document.querySelector('#turnDisplay'); //displays current player name
+       
+    //input player names, play button, clear button
     const displayInputForm = () => {
         
         const input1 = document.createElement('input');
         const input2 = document.createElement('input');
+        const playBtn = document.createElement('button');
         const clearBtn = document.createElement('button');
         const div = document.querySelector('.player-form');
 
-        input1.setAttribute('id','player1');
-        input2.setAttribute('id','player2');
+        input1.id = 'player1';
+        input2.id = 'player2';
+        input1.placeholder = 'Player 1';
+        input2.placeholder = 'Player 2';
+        playBtn.id = 'playBtn';
+        clearBtn.id = 'clearBtn';
+        playBtn.textContent = 'Play';
         clearBtn.textContent = 'Clear';
         
         div.appendChild(input1);
         div.appendChild(input2);
+        div.appendChild(playBtn);
         div.appendChild(clearBtn);
 
         clearBtn.addEventListener('click',clearBoard);
-       
+        playBtn.addEventListener('click', () => {
+            if(gameboard.setPlayer().playerOne.name && gameboard.setPlayer().playerTwo.name){
+                  const displayControllerRef = displayController();
+                  displayControllerRef.clearBoard();
+                  displayControllerRef.displayBoard();
+              }
+          })
     }
 
-    const removeInputForm = () => {
-
-    }
-
+    //displays gameboard
     const displayBoard = () => {
         const cellArray = Array.from(document.getElementsByClassName('cell'));
-        let counter = 0;
-        let clickCounter = 0;
-        let selectedPlayer = gameboard.selectPlayer;
+        const div = document.createElement('div');
+        
+        let counter = 0; //to set data attribute on each cell
+        let clickCounter = 0; //to select each player alternately: Player 1 if even, Player 2 if odd
+        let selectedPlayer = gameboard.selectPlayer;                
+
+        const gameboardGrid = document.createElement('div');
+        gameboardGrid.classList.add('gameboard');
+        gameboardGridContainer.appendChild(gameboardGrid);
 
         if(cellArray.length == 0){
+            //creates nine cells on gameboard
             gameboard.gameboardArray.forEach((board)=>{
                 const cell = document.createElement('p');
-                
                 cell.classList.add('cell');
                 cell.dataset.item = counter;
+                div.textContent = selectedPlayer(clickCounter).name + "'s turn";
                 cell.addEventListener('click',() => {
                     if(!cell.textContent){
                         cell.textContent = selectedPlayer(clickCounter).sign;
-                        // cell.textContent = cell.dataset.item;
+                        div.textContent = selectedPlayer(clickCounter+1).name + "'s turn";
                         gameboard.gameboardArray[cell.dataset.item] = selectedPlayer(clickCounter).sign; 
                         clickCounter++;
                     }
-                    gameboard.result();
+                    //checks for winner
+                    if(displayWinner(gameboard.result())){
+                        clearBoard();
+                    }
                 })
+                turnDisplay.appendChild(div);
                 gameboardGrid.appendChild(cell);
-               
                 counter++;
             })
         }
-
-        //clear input boxes after clicking play
-        // gameboard.playerNameInput().input1.value = '';
-        // gameboard.playerNameInput().input2.value = '';
     }
-       
-       
-
+    //clears gameboard   
     const clearBoard = () => {
         const cellArray = Array.from(document.getElementsByClassName('cell'));
+        const div = document.getElementById('result');
+        if(turnDisplay.hasChildNodes()){
+            turnDisplay.removeChild(turnDisplay.lastChild)
+        }
+
+        if(div.hasChildNodes()){
+            div.removeChild(div.lastChild);
+        }
+
         if(cellArray.length > 0){
             for(let i = 0; i < 9; i++){
                 gameboard.gameboardArray[i] = '';
-                cellArray[i].textContent = '';
-                gameboardGrid.removeChild(gameboardGrid.lastChild);
+                cellArray[i].textContent = ''; 
             } 
+            gameboardGridContainer.removeChild(gameboardGridContainer.lastChild);
         }
-        gameboard.playerNameInput().input1.value = '';
-        gameboard.playerNameInput().input2.value = ''; 
+        // let input1 = document.getElementById('player1');
+        // let input2 = document.getElementById('player2');
+        
+        // input1.value = '';
+        // input2.value = '';
     }
-    return {displayInputForm,removeInputForm,displayBoard}
+
+    const displayWinner = (result) => {
+        if(result){
+            clearBoard();
+
+            const div = document.getElementById('result');
+            const h2 = document.createElement('h2');
+            h2.textContent = result;
+            div.appendChild(h2);
+        }
+    }
+    return {displayInputForm,displayBoard,clearBoard}
 };
 
 
 
 displayController().displayInputForm();
-gameboard.playBtn.addEventListener('click', () => {
-    
-    if(gameboard.playerNameInput().playerOne.name && gameboard.playerNameInput().playerTwo.name){
-        displayController().displayBoard();
-    }
-})
+
 
